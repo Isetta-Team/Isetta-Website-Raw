40ec77ab807cb2a83f8ad6b69d9f306aeed7ce41
Yidi-Z <zhuyidiwow@gmail.com>
2018-11-12 23:36:37 -0500

    Added scroll top

--- docs/extra.js
+++ docs/extra.js
@@ -5,4 +5,15 @@ window.onload = function() {
             window.open(this.src);
         }
     }
+
+
+
+    var scrollToTops = document.getElementsByClassName("back_to_top");
+    for(var i = 0; i < scrollToTops.length; i++) {
+        // var scrollToTop = ;
+        scrollToTops[i].onclick = function() {
+            document.body.scrollTop = 0; // For Safari
+			document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+        }
+    }
 }
